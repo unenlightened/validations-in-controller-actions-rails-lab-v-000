9568a1cc317b0e7b11afa0be97a1e5e32224992b
Chely Ho <adept_inept@yahoo.com>
2018-04-09 23:48:16 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
 
-    render new if !@post.valid?
+    render new if @post.invalid?
     redirect_to post_path(@post)
   end
 
