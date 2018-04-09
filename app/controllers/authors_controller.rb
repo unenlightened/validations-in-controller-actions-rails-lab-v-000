19d66cb117b68f475b0b0178d6c59902041ce167
Chely Ho <adept_inept@yahoo.com>
2018-04-09 23:59:31 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,7 +9,7 @@ class AuthorsController < ApplicationController
   def create
     @author = Author.create(author_params)
 
-    if @author.invalid
+    if @author.invalid?
       render :new
     else
       redirect_to author_path(@author)
